aef56e99d8b2f40092b43ba2a86015545a812257
Joneskim <joneskimaminiel@gmail.com>
2024-01-14 14:11:22 -0600

    Did the routing again

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './components/HomePage';
 import SimulatorPage from './components/QuantumSimulator';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/simulator" element={<SimulatorPage />} />
-      </Routes>
-    </Router>
+    <SimulatorPage />
   );
 }
 
